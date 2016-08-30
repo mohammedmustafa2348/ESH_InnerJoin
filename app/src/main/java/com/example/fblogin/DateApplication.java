13222860ddba6c09e1b025ce9bf884ab4aa42766
Mohammed Mustafa <mohd.mustafa488@gmail.com>
2016-08-31 01:39:45 +0200

    DateApp
    Initial commit with FB login setup

---
+++ app/src/main/java/com/example/fblogin/DateApplication.java
@@ -0,0 +1,39 @@
+package com.example.fblogin;
+
+import android.app.Application;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.util.Base64;
+import android.util.Log;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+
+public class DateApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+/*      try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.example.fblogin",  // replace with your unique package name
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.e("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
+*/
+    }
+}
---
+++ app/src/main/java/com/example/fblogin/User.java
@@ -0,0 +1,11 @@
+package com.example.fblogin;
+
+/**
+ * Created by Mustafa on 8/30/2016.
+ */
+public class User {
+    public String name;
+    public String email;
+    public String facebookID;
+    public String gender;
+}
